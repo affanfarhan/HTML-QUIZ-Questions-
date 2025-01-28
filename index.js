a58de56abd6ff628d4dd65cd7d42d8c83c83b09b
Affan Farhan <affanfarhanfarhaniqbal@gmail.com>
2025-01-28 14:35:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+let allQuestion = [
+
+
+    {
+        question : "HTML Stand For?",
+        Option : [
+            {answer : "Hypertext Makeup Language" , iscorrect : false},
+            {answer : "Hypertext Make Language" , iscorrect : false},
+            {answer : "Hypertext Markeup Language" , iscorrect : true},
+        ]
+    },
+    
+    {
+        question : "HTML and HTML Difference?",
+        Option : [
+            {answer : "All elements supported", iscorrect : false},
+            {answer : "HTML does not supported audio iamge elements", iscorrect : true},
+            {answer : "HTML does supported audio iamge elements",iscorrect : false},
+           
+            
+            
+         ]
+
+    },
+]
